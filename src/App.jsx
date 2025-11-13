56ef3644d725d15d410bbe6ad6964550b9172708
TemmyJewel <temmyjewel22@gmail.com>
2025-11-13 11:50:46 +0100

    more details

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,77 @@
 
 import './App.css'
+import user from "./assets/user.png";
+import robot from "./assets/robot.png";
+
 
 function ChatInput(){
   return (
-    <div>
-      <input/>
+    <>
+      <input 
+        placeholder='Send a message to Chatbot' 
+        size="30"
+      />
+      
       <button>Send</button>
+    </>
+    
+  )
+}
+
+function ChatMessage({message, sender}){
+  // const message = props.message;
+  // const sender = props.sender;
+  // const { message, sender } = props;
+
+  // if(sender === 'robot'){
+  //   return (
+  //   <div>
+  //     <img src={user} width="50"/>
+  //     {message}
+  //   </div>
+    
+  // )
+  // }
+  
+  return (
+    <div>
+      {sender === 'robot' && (
+        <img src={robot} width="50"/>
+      )}
+      
+      {message} 
+      
+      {sender === 'user' && (
+        <img src={user} width="50"/>
+      )} 
+      
     </div>
     
   )
 }
+
 function App() {
   
 
   return (
     <>
-     <ChatInput />
+      <ChatInput />
+      <ChatMessage  
+        message="Hello Chatbot" 
+        sender="user"
+      />
+      <ChatMessage  
+        message="Hello! How can I help you today?" 
+        sender = "robot"
+      />
+      <ChatMessage  
+        message="Can you get me today's date?" 
+        sender="user"
+      />
+      <ChatMessage  
+        message="Today is Friday the 13th" 
+        sender = "robot"
+      />
     </>
   )
 }
